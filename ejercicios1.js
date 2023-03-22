b49c09bb5471ca109870821e0ccc9b2c1c8b69d6
agu-am <agustin.cs96@gmail.com>
2023-03-22 13:35:13 -0300

    first commit

---
+++ ejercicios1.js
@@ -0,0 +1,53 @@
+// 1) Programa una función que cuente el número de caracteres de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá 10.
+
+function contarCaracter(cadena) {
+    if (typeof cadena === "string") {
+        console.log(`La cadenada "${cadena}" tiene ${cadena.length} caracteres.`);
+    } else {
+        console.log("El texto ingresado es incorrecto.");
+    }
+}
+
+
+contarCaracter("Hola mama")
+
+
+// 2) Programa una función que te devuelva el texto recortado según el número de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola".
+
+function cortarTexto(cadena, longitud) {
+    if (typeof cadena === "string" && typeof longitud === "number") {
+        console.log(`${cadena.slice(0, longitud)}`);
+    } else {
+        console.log("Valores ingresados incorrectos");
+    }
+}
+cortarTexto("Hola mundo", 4)
+
+// 3) Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter, pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal'].
+
+function transformarCadena(cadena, separador) {
+    if (typeof cadena === "string") {
+        console.log(cadena.split(separador));
+    } else {
+        console.log("Ingrese un texto");
+    }
+}
+
+transformarCadena("Hola mundo", " ")
+
+// 4) Programa una función que repita un texto X veces, pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo.
+
+function repetirTexto(texto, repeticiones) {
+    if (typeof texto !== "string") {
+        console.log("No introduciste un texto");
+    }
+    if (typeof repeticiones !== "number") {
+        console.log("No intrduciste un numero");
+    }
+    if (repeticiones <= 0) {
+        console.log("El numero debe ser positivo");
+    } else {
+        console.log(texto.repeat(repeticiones));
+    }
+}
+repetirTexto("hOLA ", 2)
