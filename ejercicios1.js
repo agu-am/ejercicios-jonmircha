2a958159faa37fd32ab41e65fcabe09e695417d0
agu-am <agustin.cs96@gmail.com>
2023-03-30 10:27:55 -0300

    validaciones ejercicio 21 y 22

--- ejercicios1.js
+++ ejercicios1.js
@@ -287,18 +287,31 @@ validarCorreo("agustin.cs96@gmail.com")
 // 21) Programa una función que dado un array numérico devuelve otro array con los números elevados al cuadrado, pe. mi_funcion([1, 4, 5]) devolverá [1, 16, 25].
 
 const arrayAlCuadrado = (numeros) => {
+    if (!(numeros instanceof Array)) return console.log('No ingresaste un arreglo');
+    if (numeros.length === 0) return console.log('Ingresaste un arreglo vacio');
+
+    for (let num of numeros) {
+        if (typeof num !== 'number') return console.log(`El valor ${num} no es un numero`);
+    }
     return console.log(numeros.map(num => num * num));
 }
 arrayAlCuadrado([1, 4, 5])
 
 // 22) Programa una función que dado un array devuelva el número mas alto y el más bajo de dicho array, pe. miFuncion([1, 4, 5, 99, -60]) devolverá [99, -60].
 const maxMinArray = (numeros) => {
+    if (!(numeros instanceof Array)) return console.log('No ingresaste un arreglo');
+    if (numeros.length === 0) return console.log('Ingresaste un arreglo vacio');
+
+    for (let num of numeros) {
+        if (typeof num !== 'number') return console.log(`El valor ${num} no es un numero`);
+    }
+
     const max = Math.max(...numeros);
     const min = Math.min(...numeros);
-    return console.log([max,min]);
-  }
+    return console.log([max, min]);
+}
 
-  maxMinArray([1,4,5,99,-60])
+maxMinArray(["aa", 1, 3, 54,])
 
 // 23) Programa una función que dado un array de números devuelva un objeto con 2 arreglos en el primero almacena los números pares y en el segundo los impares, pe. miFuncion([1,2,3,4,5,6,7,8,9,0]) devolverá {pares: [2,4,6,8,0], impares: [1,3,5,7,9]}.
 
@@ -306,7 +319,7 @@ const ordenarParImpar = (numeros) => {
     const pares = numeros.filter(num => num % 2 === 0)
     const impares = numeros.filter(num => num % 2 !== 0)
 
-    return console.log({pares, impares}); 
+    return console.log({ pares, impares });
 }
 
-ordenarParImpar([1,2,3,4,5,6,7,8,9,0])
+ordenarParImpar([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
